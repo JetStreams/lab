df51d89859924dc15ca120faec7c007aaa5079a9
schroeder <ma.schroed73@dudmail.com>
2013-11-29 16:29:53 +0100

    test for coordinates converter extended

--- src/test/java/de/science/hack/CoordinatesConverterTest.java
+++ src/test/java/de/science/hack/CoordinatesConverterTest.java
@@ -6,6 +6,10 @@
  */
 package de.science.hack;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -14,9 +18,12 @@ import static org.junit.Assert.*;
  * @author Mario
  */
 public class CoordinatesConverterTest {
-   
+
     private static final double DIFF = 0.1;
 
+    /**
+     * simple test from coordinate to xyz-point
+     */
     @Test
     public void testToStl() {
         ModelPoint point = CoordinatesConverter.toModel(new Coordinate(0, 0, 0));
@@ -24,11 +31,45 @@ public class CoordinatesConverterTest {
         assertEquals(12756274.0, point.getX(), DIFF);
         assertEquals(CoordinatesConverter.RADIUS, point.getY(), DIFF);
         assertEquals(CoordinatesConverter.RADIUS, point.getZ(), DIFF);
-        
+
         point = CoordinatesConverter.toModel(new Coordinate(-180, 0, 0));
         assertNotNull(point);
         assertEquals(0.0, point.getX(), DIFF);
         assertEquals(CoordinatesConverter.RADIUS, point.getY(), DIFF);
         assertEquals(CoordinatesConverter.RADIUS, point.getZ(), DIFF);
+    }
+
+    /**
+     * Creates coordinates around one longitude and dumps the output.
+     * The dump can be plotted with gnuplot: <code>plot 'dump.txt' using 1:3 w points</code>
+     */
+    @Test
+    public void testCircle() {
+
+        String path = getClass().getResource(".").getFile();
+        File file = new File(path, "dump.txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+
+            for (int i = 0; i < 360; i++) {
+                ModelPoint point1 = CoordinatesConverter.toModel(new Coordinate(0, i, 0));
+                assertNotNull(point1);
+                ModelPoint point2 = CoordinatesConverter.toModel(new Coordinate(180, 360 - i, 0));
+                assertNotNull(point2);
+
+                writer.write(strip(point1));
+                writer.newLine();
+                writer.write(strip(point2));
+                writer.newLine();
+            }
+
+            writer.close();
+        } catch (IOException exc) {
+            fail(exc.getMessage());
+        }
+    }
+
+    private String strip(ModelPoint point) {
+        String asString = point.toString();
+        return asString.replace("(", "").replace(")", "").trim();
     }
 }
