f10264734b5753b47a356d309d47d654b0cfa4a0
Mario-S <spindizzy@outlook.com>
2014-08-07 22:50:44 +0200

    added check if files are empty

--- cli/src/main/java/de/science/hack/Main.java
+++ cli/src/main/java/de/science/hack/Main.java
@@ -36,59 +36,62 @@ public class Main {
     private final WindModelBuilder windModelBuilder;
     private final JetStreamModelWriter jetStreamModelWriter;
 
-
     public Main() {
         parser = new GnuParser();
-        
+
         options = new Options();
         options.addOption(buildOption(CliArg.D));
         options.addOption(buildOption(CliArg.F));
         options.addOption(buildOption(CliArg.O));
-        
+
         windModelBuilder = new WindModelBuilder();
         jetStreamModelWriter = new JetStreamModelWriter();
     }
-    
+
     private Option buildOption(CliArg arg) {
         return withArgName(arg.getLongKey()).hasArg().withDescription(arg.getDescription()).create(arg.getShortKey());
     }
 
     /**
      * This method process the arguments.
+     *
      * @param args
-     * @throws ParseException 
+     * @throws ParseException
      */
-    private void process(String [] args) throws ParseException {
+    private void process(String[] args) throws ParseException {
         CommandLine commandLine = parser.parse(options, args);
-        
-        getInputFiles(commandLine).stream().forEach((f) -> {
-            TriangleMesh mesh = windModelBuilder.build(f);
-            jetStreamModelWriter.addWindModel(mesh);
-        });
-        
-        jetStreamModelWriter.write(getOutput(commandLine));
+        Collection<File> inputFiles = getInputFiles(commandLine);
+
+        if (!inputFiles.isEmpty()) {
+            inputFiles.stream().forEach((f) -> {
+                TriangleMesh mesh = windModelBuilder.build(f);
+                jetStreamModelWriter.addWindModel(mesh);
+            });
+
+            jetStreamModelWriter.write(getOutput(commandLine));
+        }
     }
-    
-    private Collection<File> getInputFiles(CommandLine commandLine){
-       Collection<File> files = new ArrayList<>();
-       if(commandLine.hasOption(CliArg.F.getShortKey())){
-           String fileName = commandLine.getOptionValue(CliArg.F.getShortKey());
-           files.add(new File(fileName));
-       }else if(commandLine.hasOption(CliArg.D.getShortKey())){
-           String dirName = commandLine.getOptionValue(CliArg.D.getShortKey());
-           File directory = new File(dirName);
-           files.addAll(listFiles(directory, EXT, false));
-       }
-       return files;
+
+    private Collection<File> getInputFiles(CommandLine commandLine) {
+        Collection<File> files = new ArrayList<>();
+        if (commandLine.hasOption(CliArg.F.getShortKey())) {
+            String fileName = commandLine.getOptionValue(CliArg.F.getShortKey());
+            files.add(new File(fileName));
+        } else if (commandLine.hasOption(CliArg.D.getShortKey())) {
+            String dirName = commandLine.getOptionValue(CliArg.D.getShortKey());
+            File directory = new File(dirName);
+            files.addAll(listFiles(directory, EXT, false));
+        }
+        return files;
     }
- 
-    
+
     /**
      * This method returns the output file from the command line.
+     *
      * @param commandLine
-     * @return 
+     * @return
      */
-    private String getOutput(CommandLine commandLine){
+    private String getOutput(CommandLine commandLine) {
         String val = commandLine.getOptionValue(CliArg.O.getShortKey());
         return (val == null) ? getDefaultOut() : val;//return default if not provided
     }
@@ -97,7 +100,7 @@ public class Main {
         File file = new File(System.getProperty("user.dir"), DEFAULT_OUT);
         return file.getPath();
     }
-    
+
     public static void main(String[] args) throws ParseException {
         Main m = new Main();
         m.process(args);
