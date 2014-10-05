71faabb8ffecd54c13d68e874de2a11b224ead33
Mario-S <spindizzy@outlook.com>
2014-10-05 21:39:54 +0200

    fixed import

--- core/src/test/java/de/science/hack/FileModelWriterTest.java
+++ core/src/test/java/de/science/hack/FileModelWriterTest.java
@@ -11,7 +11,6 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import toxi.geom.mesh.Mesh3D;
-import toxi.geom.mesh.STLReader;
 import toxi.geom.mesh.TriangleMesh;
 
 /**
